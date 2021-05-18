659a3a0bea628045dcb8e7a36d6b9b5c9b17f62f
Marilyn-Git-It <69641390+Marilyn-Git-It@users.noreply.github.com>
2021-05-18 11:26:45 -0500

    Newsletter Sign-Up
    Plain website where you can enter your name and email to sign-up for a mailing list.

--- app.js
+++ app.js
@@ -1,42 +1,75 @@
 const express = require('express');
-const https = require('https');
 const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
-  const query = req.body.cityName;
-  const apiKey = "8150e8c14a82a62c0d8897c8e0d33786";
-  const unit = "imperial"
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  https.get(url, function(response) {
-    console.log(response.statusCode);
+  const data = {
+    members: [{
+      email_address:email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAMR: lastName
+      }
+    }]
+  };
 
-    response.on("data", function(data) {
-      const weatherData = JSON.parse(data)
-      const temp = weatherData.main.temp
-      const weatherDescription = weatherData.weather[0].description
-      const icon = weatherData.weather[0].icon
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-      res.write("<p>The weather is currently " + weatherDescription + ". </p>")
-      res.write("<h1>The temperature in "+ query + " is " + temp + " degrees Fahrenheit.</h1>")
-      res.write("<img src=" + imageURL + ">");
-      res.send();
-    })
-  });
+  const jsonData = JSON.stringify(data);
 
-});
+  const url = "https://us1.api.mailchimp.com/3.0/lists/c4717145ba";
 
+  const options = {
+    method: "POST",
+    auth: "marilyn1:2afc3f0092ae2ffed05cbbfd174f73ba-us1"
+  }
 
+const request = https.request(url, options, function(response){
 
+if(response.statusCode === 200){
+  // res.send("Successfully subscribed!");
+  res.sendFile(__dirname + "/success.html");
+} else {
+  // res.send("There was an error signing up, please try again!");
+  res.sendFile(__dirname + "/failure.html");
+}
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000.");
+response.on("data", function(data){
+  console.log(JSON.parse(data));
 })
+});
+
+request.write(jsonData);
+request.end();
+});
+
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.port || 3000, function(){
+  console.log("Server is running on port 3000");
+});
+
+
+// replace 3000 with process.env.PORT to use with Heroku
+
+// API Key
+// 2afc3f0092ae2ffed05cbbfd174f73ba-us1
+
+// List id
+// c4717145ba
