9c23d2d3a67c88826b6b1bac75b917d4cc3451d2
Marilyn-Git-It <69641390+Marilyn-Git-It@users.noreply.github.com>
2021-05-18 11:17:38 -0500

    Weather Project
    Type in the city of your choice and find out what the weather is.

--- app.js
+++ app.js
@@ -1,203 +1,42 @@
-//jshint esversion:6
-
-require("dotenv").config();
 const express = require('express');
+const https = require('https');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
-const findOrCreate = require('mongoose-findorcreate');
-
 
 const app = express();
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(session({
-  secret: "This is a secret.",
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.set("useCreateIndex", true);
-
-const userSchema = new mongoose.Schema ({
-  email: String,
-  password: String,
-  googleId: String,
-  facebookId: String,
-  secret: String
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
+  const apiKey = "8150e8c14a82a62c0d8897c8e0d33786";
+  const unit = "imperial"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
-const User = new mongoose.model("User", userSchema);
+  https.get(url, function(response) {
+    console.log(response.statusCode);
 
-passport.use(User.createStrategy());
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-console.log(profile);
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
-
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
-
-app.get("/", function(req,res){
-  res.render("home")
-});
-
-app.get("/auth/google",
-  passport.authenticate("google", {scope:["profile"]})
-);
-
-app.get("/auth/google/secrets",
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect to secret.
-    res.redirect('/secrets');
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<p>The weather is currently " + weatherDescription + ". </p>")
+      res.write("<h1>The temperature in "+ query + " is " + temp + " degrees Fahrenheit.</h1>")
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    })
   });
 
-  app.get('/auth/facebook',
-  passport.authenticate('facebook')
-);
-
-app.get('/auth/facebook/secrets',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/secrets');
-  });
-
-app.get("/login", function(req, res){
-  res.render("login");
-});
-
-app.get("/register", function(req, res){
-  res.render("register");
-});
-
-app.get("/secrets", function(req, res){
-  User.find({"secret": {$ne:null}}, function(err, foundUsers){
-    if(err){
-      console.log(err);
-    } else {
-      if(foundUsers){
-        res.render("secrets", {usersWithSecrets: foundUsers});
-      }
-    }
-  });
-});
-
-app.get("/submit", function(req, res){
-  if (req.isAuthenticated()){
-    res.render("submit");
-  } else{
-    res.redirect("/login");
-  }
-});
-
-app.post("/submit", function(req, res){
-  const submittedSecret =req.body.secret;
-
-  console.log(req.user.id);
-
-  User.findById(req.user.id, function (err, foundUsers){
-    if(err){
-      console.log(err);
-    } else {
-      if (foundUsers) {
-        foundUsers.secret = submittedSecret;
-        foundUsers.save(function(){
-          res.redirect("/secrets");
-        });
-      }
-    }
-  });
-});
-
-app.get("/logout", function(req, res){
-  req.logout();
-  res.redirect("/");
-});
-
-app.post("/register", function(req, res){
-
-User.register({username: req.body.username}, req.body.password, function(err, user){
-  if(err){
-    console.log(err);
-    res.redirect("/register");
-  } else {
-    passport.authenticate("local")(req, res, function(){
-      res.redirect("/secrets");
-    });
-  }
-});
-
-});
-
-app.post("/login", function(req, res){
-
-const user = new User({
-  username: req.body.username,
-  password: req.body.password
-});
-
-req.login(user, function(err){
-  if(err){
-    console.log(err);
-  } else{
-    passport.authenticate("local")(req, res, function(){
-      res.redirect("/secrets");
-    });
-  }
-});
-
 });
 
 
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
-});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+})
